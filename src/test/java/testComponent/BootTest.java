8577baf181f7d33bb71335b92c546230fc119181
Steven Rutherford <steven.rutherford@slalom.com>
2019-03-13 18:44:20 -0400

    another commit

--- src/test/java/testComponent/BootTest.java
+++ src/test/java/testComponent/BootTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class BootTest {
-    private Boot boot = new Boot();
+    private final Boot boot = new Boot();
 
     @Test
     public void testCorrect() {
